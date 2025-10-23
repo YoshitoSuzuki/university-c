a926404dbdef7f8731223377ad4626ccbca980a9
Yoshito Suzuki <yoshito@suzuki.one>
2025-10-23 13:21:24 +0900

    add 1023

---
+++ 1023/YoshitoSuzuki20251023.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define M 2
+#define K 3
+#define N 2
+
+
+int main(void) {
+    int A[M][K] = {{1, 2, 3}, {4, 5, 6}};
+    int B[K][N] = {{1, 2}, {3, 4}, {5, 6}};
+
+    int AB[M][N];
+
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            AB[i][j] = 0;
+            for (int k = 0; k < K; k++) {
+                AB[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d ", AB[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ 1023/script1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+    char A = 65;
+    printf("%d\t%c\n", A, A);
+    return 0;
+}
