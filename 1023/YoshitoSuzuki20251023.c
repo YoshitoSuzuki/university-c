d27f2b95aa4d4cda1c1cd141c4950c8d18411173
Yoshito Suzuki <yoshito@suzuki.one>
2025-10-23 14:10:50 +0900

    change

--- 1023/YoshitoSuzuki20251023.c
+++ 1023/YoshitoSuzuki20251023.c
@@ -5,22 +5,30 @@
 
 
 int main(void) {
-    int A[M][K] = {{1, 2, 3}, {4, 5, 6}};
-    int B[K][N] = {{1, 2}, {3, 4}, {5, 6}};
+    int A[M][K] = {
+        {1, 2, 3}, 
+        {4, 5, 6}
+    };
+
+    int B[K][N] = {
+        {1, 2}, 
+        {3, 4}, 
+        {5, 6}
+    };
 
     int AB[M][N];
 
-    for (int i = 0; i < M; i++) {
-        for (int j = 0; j < N; j++) {
+    for (int i=0; i<M; i++) {
+        for (int j=0; j<N; j++) {
             AB[i][j] = 0;
-            for (int k = 0; k < K; k++) {
+            for (int k=0; k<K; k++) {
                 AB[i][j] += A[i][k] * B[k][j];
             }
         }
     }
 
-    for (int i = 0; i < M; i++) {
-        for (int j = 0; j < N; j++) {
+    for (int i=0; i<M; i++) {
+        for (int j=0; j<N; j++) {
             printf("%d ", AB[i][j]);
         }
         printf("\n");
---
+++ 1023/ex4-4.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#define N 12
+#define M 20
+#define S 2
+
+int main(void) {
+    char input;
+    char maze[N][M] = {
+        "####################",
+        "#S          ##     #",
+        "# #####   ####   ###",
+        "# ##      ##       #",
+        "# ##  ######  ##   #",
+        "####  ##       #   #",
+        "#     ##  ###  #   #",
+        "#  #####  ###  #   #",
+        "#         ###  #  G#",
+        "####################",
+        "######stage 1#######",
+        "####################"
+    };
+
+    int start[2] = {1, 1};
+    int goal[2] = {8, 18};
+    int player[2] = {start[0], start[1]};
+
+    while (1) {
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<M; j++) {
+                printf("%c", maze[i][j]);
+            }
+            printf("\n");
+        }
+
+        if (player[0] == goal[0] && player[1] == goal[1]) {
+            printf("goal!\n");
+            break;
+        }
+
+        printf("W/A/S/D: ");
+        scanf(" %c", &input);
+
+        int nextLocation[2] = {player[0], player[1]};
+
+        if (input == 'W' || input == 'w') {
+            nextLocation[0]--;
+        }else if (input == 'S' || input == 's') {
+            nextLocation[0]++;
+        }else if (input == 'A' || input == 'a') {
+            nextLocation[1]--;;
+        }else if (input == 'D' || input == 'd') {
+            nextLocation[1]++;;
+        }else {
+            printf("Enter W/A/S/D\n");
+        }
+
+        if (maze[nextLocation[0]][nextLocation[1]] != '#') {
+            maze[player[0]][player[1]] = ' ';
+            maze[start[0]][start[1]] = 'S';
+            maze[goal[0]][goal[1]] = 'G';
+            player[0] = nextLocation[0];
+            player[1] = nextLocation[1];
+            maze[player[0]][player[1]] = '@';
+        }
+    }
+
+    return 0;
+}
